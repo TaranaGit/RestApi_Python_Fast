fd243705447c1e7aaa8605807a740089b3dba535
Tarana <tarana.ara@ontariotechu.net>
2024-02-05 19:49:11 -0700

    made some changes on put api

--- routes/user.py
+++ routes/user.py
@@ -25,21 +25,21 @@ async def create_user(user:User):
 async def delete_user(id, user:User):
     return userEntity (db["py_mongo_collection"].delete_one({"_id":ObjectId(id)})) 
 
-# @user.put('/api/{id}')
-# async def update_user(id, user:User):
-#     db["py_mongo_collection"].find_one_and_update({"_id":ObjectId(id)}, {"$set":dict(user)})
-#     return userEntity( db["py_mongo_collection"].find_one({"_id":ObjectId(id)}))
+@user.put('/api/{id}')
+async def update_user(id, user:User):
+    db["py_mongo_collection"].find_one_and_update({"_id":ObjectId(id)}, {"$set":dict(user)})
+    return userEntity( db["py_mongo_collection"].find_one({"_id":ObjectId(id)}))
 
 # Define route for updating a user
-@user.put('/api/{id}')
-async def update_user(id: str, user: User):
-    # Retrieve existing user data from MongoDB
-    existing_user = db["py_mongo_collection"].find_one({"_id": ObjectId(id)})
-    if existing_user:
-        # Create a dictionary containing only the fields that are not None in the request
-        update_fields = {key: value for key, value in user.model_dump().items() if value is not None}
-        # Update the user with the specified fields
-        db["py_mongo_collection"].update_one({"_id": ObjectId(id)}, {"$set": update_fields})
-        # Retrieve and return the updated user entity
-        updated_user = db["py_mongo_collection"].find_one({"_id": ObjectId(id)})
-        return updated_user
+# @user.put('/api/{id}')
+# async def update_user(id: str, user: User):
+#     # Retrieve existing user data from MongoDB
+#     existing_user = db["py_mongo_collection"].find_one({"_id": ObjectId(id)})
+#     if existing_user:
+#         # Create a dictionary containing only the fields that are not None in the request
+#         update_fields = {key: value for key, value in user.model_dump().items() if value is not None}
+#         # Update the user with the specified fields
+#         db["py_mongo_collection"].update_one({"_id": ObjectId(id)}, {"$set": update_fields})
+#         # Retrieve and return the updated user entity
+#         updated_user = db["py_mongo_collection"].find_one({"_id": ObjectId(id)})
+#         return updated_user
